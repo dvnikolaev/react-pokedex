7d6f38ec2d25bab853c0207a50234d0c8bf8b064
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-19 20:23:45 +0400

    Updates

---
+++ precache-manifest.d5c8be9edec165f084c9d0fd742a4334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5607f4a9a484cfd6092be675da912834",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "bb1a9f469e378055e7c0",
+    "url": "/react-pokedex/static/css/main.6456b815.chunk.css"
+  },
+  {
+    "revision": "584d8e1ad478d202fc9a",
+    "url": "/react-pokedex/static/js/2.01f6c0ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pokedex/static/js/2.01f6c0ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1a9f469e378055e7c0",
+    "url": "/react-pokedex/static/js/main.a4d93c58.chunk.js"
+  },
+  {
+    "revision": "8d0dc51f96d551a6d0f3",
+    "url": "/react-pokedex/static/js/runtime-main.f27a760f.js"
+  }
+]);
