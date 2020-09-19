8e1c6cfec9b69a78f161cdeaeea88c5c17ca1a62
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-19 21:36:26 +0400

    fix

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,7 @@ import thunk from "redux-thunk";
 
 import rootReducer from "./reducers/rootReducer";
 
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
 
 const store = createStore(rootReducer, compose(
   applyMiddleware(thunk),
