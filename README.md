19c5f735a9d1f8867d6ff0d81ffd0790984d791a
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-19 21:41:57 +0400

    add link gh-pages

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## https://dvnikolaev.github.io/react-pokedex/
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from "./reducers/rootReducer";
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
-
-const store = createStore(rootReducer, compose(
-  applyMiddleware(thunk),
-  devTools
-));
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 export default store;
